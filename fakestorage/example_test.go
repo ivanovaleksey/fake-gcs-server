7627dd8b276c58b6157cd22b9c5e228caa9045bf
Aleksei Ivanov <aleksei.ivanov@utopiamusic.com>
2023-06-07 22:53:10 +0200

    Reproduce issue

--- fakestorage/example_test.go
+++ fakestorage/example_test.go
@@ -8,6 +8,8 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"log"
+	"testing"
 
 	"github.com/fsouza/fake-gcs-server/fakestorage"
 )
@@ -51,6 +53,74 @@ func ExampleServer_with_host_port() {
 		},
 		Host: "127.0.0.1",
 		Port: 8081,
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer server.Stop()
+	client := server.Client()
+	object := client.Bucket("some-bucket").Object("some/object/file.txt")
+	reader, err := object.NewReader(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	defer reader.Close()
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", data)
+	// Output: inside the file
+}
+
+func ExampleServer_with_public_host() {
+	server, err := fakestorage.NewServerWithOptions(fakestorage.Options{
+		InitialObjects: []fakestorage.Object{
+			{
+				ObjectAttrs: fakestorage.ObjectAttrs{
+					BucketName: "some-bucket",
+					Name:       "some/object/file.txt",
+				},
+				Content: []byte("inside the file"),
+			},
+		},
+		Host:       "127.0.0.1",
+		Port:       8081,
+		PublicHost: "127.0.0.1:8081",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer server.Stop()
+	client := server.Client()
+	object := client.Bucket("some-bucket").Object("some/object/file.txt")
+	reader, err := object.NewReader(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", data)
+	// Output: inside the file
+}
+
+func TestServer_with_public_host(t *testing.T) {
+	server, err := fakestorage.NewServerWithOptions(fakestorage.Options{
+		InitialObjects: []fakestorage.Object{
+			{
+				ObjectAttrs: fakestorage.ObjectAttrs{
+					BucketName: "some-bucket",
+					Name:       "some/object/file.txt",
+				},
+				Content: []byte("inside the file"),
+			},
+		},
+		Host:       "127.0.0.1",
+		Port:       8081,
+		PublicHost: "127.0.0.1:8081",
 	})
 	if err != nil {
 		panic(err)
